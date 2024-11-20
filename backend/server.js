f43b82119cd13bcd78d01e267a85804f8303efdf
Ketan Singh <ketansingh1@districtd.in>
2024-11-20 22:43:15 +0530

    feat: dotenv setup

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,16 @@ import dotenv from "dotenv";
 const app = express();
 import authRoutes from "./routes/auth.routes.js"
 import connectMongoDB from "./db/connectMongoDB.js";
-const PORT = 3000;
 dotenv.config();
 
+
+const PORT = process.env.PORT || 5000;
+dotenv.config();
 
 //app.get looks for a callback fuction and app.use can handle post, get, put etc...
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running in ${PORT}`);
-    connectMongoDB();
-})
+  console.log(`Server is running in ${PORT}`);
+  connectMongoDB();
+});
