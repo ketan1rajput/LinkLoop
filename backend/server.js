ea19b2017d72129af527dae8d0cc4a3b7869624a
Ketan Singh <ketansingh1@districtd.in>
2024-11-22 15:33:50 +0530

    feat: user route and controllers added

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
-const app = express();
-import authRoutes from "./routes/auth.routes.js"
-import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary} from "cloudinary";
+
+import authRoutes from "./routes/auth.routes.js"
+import userRoutes from "./routes/user.routes.js"
+
+import connectMongoDB from "./db/connectMongoDB.js";
+
 dotenv.config();
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+const app = express();
 const PORT = process.env.PORT || 5000;
 
 //middleware is a function that runs between request and response
@@ -13,6 +24,7 @@ app.use(express.urlencoded({ extended: true })) //to parse from data(urlencoded)
 app.use(cookieParser());
 //app.get looks for a callback fuction and app.use can handle post, get, put etc...
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running in ${PORT}`);
